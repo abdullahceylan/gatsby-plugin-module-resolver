4b4141cbe02f843eb893afbdf5a34efb0af495ca
Abdullah Ceylan <abdullah@creativouse.co.uk>
2019-01-26 23:53:07 +0000

    :zap: Add the custom root dir support for  aliases

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
-const clear = p => p.replace('./', '').trim();
-
 exports.onCreateWebpackConfig = ({ actions, getConfig }, pluginOptions) => {
-  // get gatsby config
+  // get Gatsby's config
   const config = getConfig();
+  // trim and replace
+  const clear = p => p.replace('./', '').trim();
   const hasOptions = Object.keys(pluginOptions).filter(
     item => item !== 'plugins'
   ).length;
@@ -15,22 +15,36 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }, pluginOptions) => {
     // get user specified root dir
     const rootDir = options.root || './';
     // build the root dir
-    const contextSrc = path.join(config.context, clear(rootDir));
+    const mainRoot = path.join(config.context, clear(rootDir));
     // create an empty alias object
     const alias = {};
+    // create the alias with the params that specified
+    const createAlias = (value, root) => {
+      let finalValue = value;
+      if (finalValue.startsWith('./')) {
+        finalValue = clear(finalValue);
+      }
+      return `${root}/${finalValue}`;
+    };
 
     // clear paths and add to the alias object
-    Object.keys(options.alias).map(opt => {
-      if (options.alias[opt].startsWith('./')) {
-        alias[opt] = `${contextSrc}/${clear(options.alias[opt])}`;
+    Object.keys(options.aliases).map(opt => {
+      const value = options.aliases[opt];
+
+      // check if the value is an object
+      if (typeof value === 'object') {
+        const childRoot = path.join(config.context, clear(value.root));
+        alias[opt] = createAlias(value.alias, childRoot || mainRoot);
+      } else {
+        alias[opt] = createAlias(options.aliases[opt], mainRoot);
       }
     });
 
     // set Webpack config
     actions.setWebpackConfig({
       resolve: {
-        alias
-      }
+        alias,
+      },
     });
   }
 };
